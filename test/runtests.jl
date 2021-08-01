0d5eb1845bd5a1d31bf0900ff0ca2371d68790be
Sandeep Singh Sandhu <sandeep.sandhu@in.ibm.com>
2021-08-02 01:15:12 +0530

    Fixed failed test script on github environment 3

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,11 @@
 using Test
 
-using newslookout
+#using newslookout
+
+println(readdir("/home/runner/work/NewsLookout.jl/"))
 
 # include("../src/newslookout.jl")
 
-@test NewsLookout.greet() == "-- The NewsLookout Web Scraping App ---"
+#@test NewsLookout.greet() == "-- The NewsLookout Web Scraping App ---"
 
 @test [1, 2] + [2, 1] == [3, 3]
